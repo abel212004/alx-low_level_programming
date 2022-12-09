c7b74b75eab47d6c568aadfdea444c75f408fe7c
abel212004 <abelbirhan1996@gmail.com>
2022-12-08 16:29:31 -0800

    end of mandatory

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main is the basic function of every c program
+ * the code is set to always return 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
