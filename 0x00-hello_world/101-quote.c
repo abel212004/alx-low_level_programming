c5a922568e038eb601b50c31ca7377c82be65711
abel212004 <abelbirhan1996@gmail.com>
2022-12-08 17:26:32 -0800

    end of mandatory

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - starts the program
+ *
+ * Return: A0 successful
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful");
+	fprintf(stderr, "\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
