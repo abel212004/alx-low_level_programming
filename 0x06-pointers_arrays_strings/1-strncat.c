d40dba37dbb2f12d0a313fa8410d84e693571d58
hackable <user.name>
2022-12-22 22:23:06 +0300

    Thu Dec 22 10:23:06 PM EAT 2022

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,24 +1,24 @@
 #include "main.h"
+
 /**
  * _strncat - concatenates two strings
- * @dest: the destination of the concatenated string
- * @src: the source of the string to concatenate
- * Return: a string of characters
+ * @src: The source of strings
+ * @dest: The destination of the string
+ * @n: The length of int
+ * Return: pointer to the resulting string dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0, j = 0;
+	int i, j;
 
-	while (dest[i] != 0)
+	for (i = 0; dest[i] != '\0'; i++)
 	{
-		i++;
+		continue;
 	}
-
-	while (j < n && src[j] != 0)
+	for (j = 0; src[j] != '\0' && j < n; j++)
 	{
 		dest[i + j] = src[j];
-		j++;
 	}
-	dest[i + j] = 0;
+	dest[i + j] = '\0';
 	return (dest);
 }
