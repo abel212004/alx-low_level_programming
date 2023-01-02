bd4f71907724ff54f58e4630a513742be3608b41
abel212004 <abelbirhan1996@gmail.com>
2023-01-02 20:47:58 +0300

    date

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,11 @@
 #include "main.h"
+
 /**
+ *_memset - Fills the first n bytes of the memory area
+ *@s: A pointer of the memory area to be filled
+ *@c: The character to fill the memory area with
+ *@n: number of bytes that are going to be filled
+ *main: no main in this function
  *creating a function that works as memset
  *objective : using memset() function
  *Return : character
